2b05a2914897d964dd9270e1262cb85d10098554
Daniel Pope <daniel.pope@twosigma.com>
2018-08-03 13:33:12 +0000

    Apply rustfmt

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,9 @@
 extern crate exec;
 
-use std::path::{Path, PathBuf};
 use std::env;
-use std::process;
 use std::ffi::OsString;
-
+use std::path::{Path, PathBuf};
+use std::process;
 
 fn walk_symlink(link: &Path) -> PathBuf {
     let mut p = PathBuf::from(link);
@@ -13,17 +12,13 @@ fn walk_symlink(link: &Path) -> PathBuf {
         match p.read_link() {
             Ok(dest) => {
                 p = dest;
-            },
-            Err(_) => break p
+            }
+            Err(_) => break p,
         }
     }
 }
 
-
-fn exec_relative(
-    relcmd: OsString,
-    script: OsString,
-    argv: &[OsString]) -> Result<(), String> {
+fn exec_relative(relcmd: OsString, script: OsString, argv: &[OsString]) -> Result<(), String> {
     let relcmd = Path::new(&relcmd);
     let origin_script = walk_symlink(Path::new(&script));
 
@@ -34,28 +29,30 @@ fn exec_relative(
     cmd.arg(&script).args(argv);
 
     let err = cmd.exec();
-    Err(format!("Error executing {}: {}", target_interp.display(), err))
+    Err(format!(
+        "Error executing {}: {}",
+        target_interp.display(),
+        err
+    ))
 }
-
 
 fn main() {
     let mut argv = env::args_os();
 
     let result = match argv.len() {
-        0 ... 2 => {
-            let program = argv.next()
-                              .unwrap_or_else(|| OsString::from("relexec"));
+        0...2 => {
+            let program = argv.next().unwrap_or_else(|| OsString::from("relexec"));
             Err(format!(
                 "Usage: {} <interpreter> <file> ...",
                 Path::new(&program).display()
             ))
-        },
+        }
         _ => {
             argv.next();
             exec_relative(
                 argv.next().unwrap(),
                 argv.next().unwrap(),
-                &argv.collect::<Vec<OsString>>()
+                &argv.collect::<Vec<OsString>>(),
             )
         }
     };
